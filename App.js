186b44a198af75b8e4ce21b9711a6d0d64c71bab
GayathriPalepu <78401420+GayathriPalepu@users.noreply.github.com>
2021-02-08 16:06:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import Demo from './components/demo'
+import ClassBasedcom from './components/classBasedcomponent'
+import Hooks from './components/hooksInc'
+import Data from './data/data.json'
+import {BrowserRouter,Route,Link} from 'react-router-dom';
+import Profile from './components/profile'; 
+
+function App() {
+  var info=Data.profiles;
+  return (
+    <BrowserRouter>
+    <Route exact  path ="/myfirstapp" component={Home}/>
+    <Route exact  path ="/Profile"component={Profile}/>
+    </BrowserRouter>
+    
+
+  );
+}
+let Home=()=>{
+  var info=Data.profiles;
+  return(
+    <div className="App">
+    {info.map((i,index)=>(
+      <div key={index}>
+      
+        <h1>{i.name}</h1>
+        <h2>{i.role}</h2>
+        <Link to ={{pathname="/Profile",data:{id:index}}}>VIEW DETAILS</Link>
+     
+      </div>
+    ))}
+    
+  
+  </div>
+  );
+}
+
+
+export default App;
