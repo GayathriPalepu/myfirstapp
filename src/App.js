7c922bdebf2d289a7d9eb0800966b416b5b9e33c
Karishma5I1 <ramoji101102@gmail.com>
2021-02-08 12:23:09 +0530

    this my app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Demo from './components/demo'
+import ClassBasedcom from './components/classBasedcomponent'
+import Hooks from './components/hooksInc'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Nec</h1>
+      <Demo></Demo>
+      <Demo />
+      <Demo />
+      <Home />
+      <Hooks />
+      <h1>Hooks</h1>
+      <ClassBasedcom name="Gayathri">NEC</ClassBasedcom>
+    
     </div>
   );
 }
+let Home=()=>{
+  return(
+    <h1><i>This is my home component</i></h1>
+  );
+}
+
 
 export default App;
